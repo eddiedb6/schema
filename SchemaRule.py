d445dcdbd6deaa4b68a405f3f9996eed89c872ea
eddiedb6 <eddiedb6@gmail.com>
2016-08-23 18:04:40 +0800

    Change class member to private

--- SchemaRule.py
+++ SchemaRule.py
@@ -66,19 +66,19 @@ class BaseRule:
 
 class KeyIn(BaseRule):
     def __init__(self, collection):
-        self._collection = collection
+        self.__collection = collection
     def Check(self, key, value, schema):
         for k in value:
-            if k not in self._collection:
+            if k not in self.__collection:
                 Error("Key is not in specified collection: " + k)
                 return False
         return True
     
 class ValueIn(BaseRule):
     def __init__(self, collection):
-        self._collection = collection
+        self.__collection = collection
     def Check(self, key, value, schema):
-        for v in self._collection:
+        for v in self.__collection:
             if value == v:
                 return True
         Error("Value is not in specified collection: " + value)
@@ -86,9 +86,9 @@ class ValueIn(BaseRule):
     
 class HasKey(BaseRule):
     def __init__(self, *args):
-        self._keys = args
+        self.__keys = args
     def Check(self, key, value, schema):
-        for k in self._keys:
+        for k in self.__keys:
             if k not in value:
                 Error("Key is not defined: " + k + ", " + key)
                 return False
@@ -96,22 +96,22 @@ class HasKey(BaseRule):
 
 class AtLeastOneKey(BaseRule):
     def __init__(self, *args):
-        self._keys = args
+        self.__keys = args
     def Check(self, key, value, schema):
         for k in value:
-            if k in self._keys:
+            if k in self.__keys:
                 return True
         Error("None of required key is defined: " + key)
         return False
 
 class CheckAsTypeFromKey(BaseRule):
     def __init__(self, key):
-        self._key = key
+        self.__key = key
     def Check(self, key, value, schema):
-        if self._key not in value:
-            Error("Key of type is not defined: " + self._key, + ", " + key)
+        if self.__key not in value:
+            Error("Key of type is not defined: " + self.__key, + ", " + key)
             return False
-        typeFromKey = value[self._key]
+        typeFromKey = value[self.__key]
         if typeFromKey not in schema:
             Error("Type is not defined in schema: " + typeFromKey)
             return False
@@ -122,24 +122,24 @@ class CheckAsTypeFromKey(BaseRule):
 
 class NotEmpty(BaseRule):
     def __init__(self, dataType):
-        self._dataType = dataType
+        self.__dataType = dataType
     def Check(self, key, value, schema):
-        if self._dataType not in MetaType:
-            Error("Schema data type is not defined: " + self._dataType)
+        if self.__dataType not in MetaType:
+            Error("Schema data type is not defined: " + self.__dataType)
             return False
-        if MetaType[self._dataType][1](value):
+        if MetaType[self.__dataType][1](value):
             Error("Value is empty: " + key)
             return False
         return True
 
 class CheckForeachAsType(BaseRule):
     def __init__(self, schemaKey):
-        self._schemaKey = schemaKey
+        self.__schemaKey = schemaKey
     def Check(self, key, value, schema):
-        if self._schemaKey not in schema:
-            Error("Schema key is not defined in schema when check for each: " + self._schemaKey)
+        if self.__schemaKey not in schema:
+            Error("Schema key is not defined in schema when check for each: " + self.__schemaKey)
             return False
-        keySchema = _cloneSchema(schema[self._schemaKey], schema)
+        keySchema = _cloneSchema(schema[self.__schemaKey], schema)
         if keySchema is None:
             return False
         for v in value:
